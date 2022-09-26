d952b715c1f1dbc5a1ff3bd507f12598b8933621
Arun Jangra <arunjngra89@gmail.com>
2022-09-27 02:14:57 +0530

    Nodes 📦 completed

--- Application/contracts/provider.sol
+++ Application/contracts/provider.sol
@@ -4,44 +4,32 @@ pragma solidity >=0.6.6 <0.9.0;
 import "./main.sol";
 
 contract Provider {
-    /*
-    PROVIDER NODE :
-    - to provide the docs
-    - check the keys
-    - check if patient in chain
-    */
+    address public AddressOfProvider;
+
+    constructor() public {
+        AddressOfProvider = msg.sender;
+    }
 
     // Keeping the record of the providers in the network
     mapping(address => bool) public Providers;
 
     // Set user as a provider
-    function setProvider(address UserAddress) {
+    function setProvider(address _UserAddress) private {
         Providers[_UserAddress] = true;
     }
 
     // Check if a user address is the provider
-    function checkIfProvider(address UserAddress) returns (bool) {
+    function checkIfProvider(address _UserAddress) public view returns (bool) {
         return Providers[_UserAddress];
     }
 
     // To check if patients exists
-    function patientCheck(uint256 patientWalletAddress)
+    function patientCheck(address _patientWalletAddress)
         public
         view
         returns (bool)
-    {}
-
-    // To get the key to the 3rd party
-    function getKey(uint256 patientWalletAddress)
-        public
-        view
-        returns (string memory)
-    {}
-
-    // Provide the key to the third party to access the docs
-    function giveAccessKey(uint256 patientKey)
-        public
-        view
-        returns (string memory)
-    {}
+    {
+        Main instanceOfMain = Main(_patientWalletAddress);
+        return instanceOfMain.checkPatient(_patientWalletAddress);
+    }
 }
